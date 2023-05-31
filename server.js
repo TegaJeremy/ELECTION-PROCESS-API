9a7e323cab6d180e548cfebc8fc41c2ebcddd5a2
TegaJeremy <oghenedemartin@gmail.com>
2023-05-31 15:59:01 +0100

    first commit

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
 const express = require ("express");
 const mongoose = require ("mongoose")
-const PORT =  2050;
+const PORT =  2053;
 const data = express();
 data.use(express.json());
 
 
-const dataschema=new mongoose.Schema(
+const communityschema=new mongoose.Schema(
  {
-        
-    data:{
-        name:String,
-        class:String, 
-        course:String, 
-        designation:String, },
-        score:{
-        html:Number, 
-        css:Number, 
-        javaScript:Number, 
-        node:Number, }
-    }
+         state:String,
+         parties:[String],
+         result:{
+            apc: Number,  
+            lp:Number,
+            pdp:Number,
+            apaga:Number,
+            nnpp: Number
+           },
+         collectionOfficer:String,
+         isRigged:Boolean, 
+         totalLG:Number,
+         }
+    
 )
 
-const user =mongoose.model("Blog", dataschema)
+const user =mongoose.model("universe", communityschema)
  
-// blog.get("/",(req,res)=>{
-//     res.status(200).json("welcome to my page")
-//     // res.send("welcome to my page")
-// })
-
-
-
-//creating a data in database
+//creating a state in database
 
 data.post("/createdata", async (req,res)=>{
     const newResult = await new user (req.body);
@@ -40,29 +35,29 @@ data.post("/createdata", async (req,res)=>{
     
 })
 
-// get all data
+// get all state
 
-data.get("/getallstudent", async(req,res)=>{
+data.get("/getallstate", async(req,res)=>{
     const  all  = await user.find();
     
     res.status(200).json({
-        message:"the available user are" + all.length, data:all
+        message:"the available user are:" + all.length, data:all
     })
 })
 
-//get one
-data.get("/getone/:id" , async(req, res)=>{
+//get one  state
+data.get("/getonestate/:id" , async(req, res)=>{
     const id =req.params.id
     const getone = await user.findById(id)
     
     res.status(200).json(
-        {message:`kindly find the infomation of the user with the id of ${id}`, 
+        {message:`the infomations of this ${id} is:`, 
         data:getone}
     )
 
 })
 
-// delete a user
+// delete a state
 data.delete("/delete/:id", async(req, res)=>{
     const id = req.params.id;
     const deleteUser = await user.findByIdAndDelete((id))
@@ -75,19 +70,45 @@ data.delete("/delete/:id", async(req, res)=>{
     )
 })
 
-data.put("/editstudent/:id", async (req, res)=>{
-    const id = req.params.id;
-    const edittask = await user.findByIdAndUpdate((id))
-    res.status(201).json( {
-        message: "update successful",
-        title :edittask
-    }
+
+
+data.put( "/updatestate/:Id", async ( req, res ) => {
+    try {
+        const stateId = req.params.Id;
+        const data = await user.findById( stateId );
         
-    )
-    
-})
+        const bodyData = {
+        state: req.body.state || data.state,
+        parties: req.body.parties || data.parties,
+        result: {
+            apc: req.body.apc || data.result.apc,
+            lp: req.body.lp || data.result.lp,
+            pdp: req.body.pdp || data.result.pdp,
+            apaga: req.body.apaga || data.result.apaga,
+            nnpp: req.body.nnpp || data.result.nnpp
+            },
+            collectionOfficer: req.body.collectionOfficer || data.collectionOfficer,
+            isRiggedr: req.body.isRigged || data.isRigged,
+            totalLG:req.body.totalLG|| data.totalLG,
+        }
+
+        await user.updateOne( bodyData );
+        if ( bodyData ) {
+            res.status(200).json(bodyData);
+        } else {
+            res.status( 400 ).json( {
+                Error: 'Error updating id.'
+            })
+        }
+    } catch ( e ) {
+        res.status( 400 ).json( {
+            Message: e.message
+        })
+    }
+} )
 
 
+        
 
 mongoose.connect("mongodb+srv://oghenedemartin:eQX78GsvMNFP2p44@cluster0.rivgmxb.mongodb.net/")
 .then(()=>{
